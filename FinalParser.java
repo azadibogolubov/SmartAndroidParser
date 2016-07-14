cdf744aef13cfa3ca926f15fa170502f13e00acc
Azadi Bogolubov <Azadi.Bogolubov@cigna.com>
2016-07-14 14:09:48 -0700

    Restructuring

--- FinalParser.java
+++ FinalParser.java
@@ -24,19 +24,39 @@ public class FinalParser {
 		    boolean nestedElement = false;
 		    String[] currentLine = null;
 		    while ((line = br.readLine()) != null) {
+		    	System.out.println("Line = " + line);
 		    	if (line.contains("\t")) {
 		    		currentLine = line.split("\t");
 		    		currentElement.add(currentLine[1]);
 		    		System.out.println("After tab: " + currentLine[1]);
 		    	}
-		    	if (currentLine != null && line.equals(types.get(0)))
+		    	else  {
+		    		currentLine = new String[] { "LinearLayout" };
+		    	}
+		    	System.out.println("CurrentLine = " + currentLine[0]);
+		    	if (currentLine.length > 1) {
+		    		System.out.println("CurrentLine[1] = " + currentLine[1]);
+		    	}
+		    	if (currentLine.equals(types.get(0)))
 		    	{
+		    		System.out.println("LL");
 		    		processType("LinearLayout");
 			    	count++;
 		       }
-		       else if (line.equals(types.get(0))) {
-		      		processType("LinearLayout");
+		       else if (currentLine.equals(types.get(1))) {
+		    		System.out.println("RL");
+		      		processType("RelativeLayout");
 		      		count++;
+		       }
+		       else if (currentLine.equals(types.get(2))) {
+		       		System.out.println("TV");
+		       		processType("TextView");
+		       		count++;
+		       }
+		       else if (currentLine.equals(types.get(3))) {
+		       		System.out.println("ET");
+		       		processType("EditText");
+		       		count++;
 		       }
 		    }
 		}
@@ -54,6 +74,15 @@ public class FinalParser {
 		switch (type) {
 			case "LinearLayout":
 				System.out.println("<LinearLayout\n\tandroid:layout_width=\"wrap_content\"\n\tandroid:layout_height=\"wrap_content\">\n</LinearLayout>");
+				break;
+			case "RelativeLayout":
+				System.out.println("<RelativeLayout\n\tandroid:layout_width=\"wrap_content\"\n\tandroid:layout_height=\"wrap_content\">\n</RelativeLayout>");
+				break;
+			case "TextView":
+				System.out.println("<TextView\n\tandroid:layout_width=\"wrap_content\"\n\tandroid:layout_height=\"wrap_content\">\n</TextView>");
+				break;
+			case "EditText":
+				System.out.println("<EditText\n\tandroid:layout_width=\"wrap_content\"\n\tandroid:layout_height=\"wrap_content\">\n</EditText>");
 				break;
 			default: 
 				System.out.println("Invalid type");
